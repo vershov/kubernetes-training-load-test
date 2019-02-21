74eab8d9edeef8710b4fede6b07845ad6b7719c2
Florian Assmus <florian.assmus@prodyna.com>
2019-02-21 13:44:31 +0100

    Set options

--- tomcat_get.js
+++ tomcat_get.js
@@ -1,5 +1,11 @@
 import http from 'k6/http';
 
+export let options = {
+    duration: "10m",
+    vus: 10,
+    rps: 500
+  };
+
 export default function() {
   const response = http.get("http://tomcat-sample");
 };
